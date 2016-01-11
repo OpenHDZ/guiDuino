7adc86d91bfb0849122b4f34f426de829ae3fd6e
areour <openhardwaredz@gmail.com>
2016-01-11 09:29:29 +0100

    mise à jour

--- GuiDuino/GuiDuino.py
+++ GuiDuino/GuiDuino.py
@@ -1,8 +1,25 @@
 # -*- coding: cp1252 -*-
-# GuiDuino interface graphique pour Arduino
-# Version : 0.01 
-# Ecrit par Areour mohamed Cherif
-# date : 27/04/2014
+
+"""
+Exemple d'utilisation du Galvanomètre développé pour la librairie Tkinter
+Ecrit par Areour mohamed Cherif
+Date : 27/04/2014
+E-mail : openhardwaredz@gmail.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
 
 from Tkinter import *
 import serial
--- GuiDuino/moderneGauge.py
+++ GuiDuino/moderneGauge.py
@@ -1,23 +1,52 @@
+# -*- coding: cp1252 -*-
+
+"""
+Gauge développé pour la librairie Tkinter
+Ecrit par Areour mohamed Cherif
+Date : 10/01/2016
+E-mail : openhardwaredz@gmail.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
 import Tkinter
 import time
 from random import randrange
 
 class Mod_gauge(Tkinter.Canvas):
-    def __init__(self, parent):
-        global valeur, root, arc, text, H, W
+    def __init__(self, parent, titre = "Gauge"):
+        global valeur, root, arc, text, H, W, coord1, coord2
 
+        self.titre = titre
         root = parent
         valeur = 0.
-        H=260
+        H=290
         W=260
+        coord1 = (H-240), (W-215), (H-80), (W-50)
+        coord2 = (H-270), (W-240), (H-50), (W-20)
 
+        Tkinter.Canvas.__init__(self, bg="#FF2E2E", height=H, width=W)
 
-        Tkinter.Canvas.__init__(self, bg="#3300FF", height=H, width=W)
         # Dessin de la gauge
-        self.create_oval((H-210), (W-215), (H-50), (W-50), fill="#3300FF", outline="#3300FF")       
+        self.create_oval(coord1, fill="#FF2E2E", outline="#FF8B8B")
+        #self.create_oval(coord1, outline="#3399FF")
 
-        arc = self.create_arc((H-240), (W-240), (H-20), (W-20), start=90, extent = valeur, fill="#3399FF",outline="#3399FF")    
-        text = self.create_text(130, 130, text=int(valeur), font="Arial 40 italic", fill="#3399FF")
+        arc = self.create_arc(coord2, start=90, extent = valeur, fill="#FF8B8B",outline="#FF8B8B")
+        self.create_oval(coord2, outline="#FF8B8B")
+        text = self.create_text(130, 130, text=int(valeur), font="Arial 40 italic", fill="#FF8B8B")
+        legende = self.create_text(130, 260, text= self.titre, font="Arial 20 ", fill="#FF8B8B")
         
         parent.update()
 
@@ -32,36 +61,43 @@ class Mod_gauge(Tkinter.Canvas):
                 txt_consigne = valeur/3.6
                 self.delete(arc)
                 self.delete(text)
-                arc = self.create_arc((H-240), (W-240), (H-20), (W-20), start=90, extent=-valeur, fill="#3399FF",outline="#3399FF")
-                self.create_oval((H-210), (W-215), (H-50), (W-50), fill="#3300FF", outline="#3300FF")
-                text = self.create_text(130, 130, text=int(txt_consigne), font="Arial 40 italic", fill="#3399FF")
+                arc = self.create_arc(coord2, start=90, extent=-valeur, fill="#FF8B8B")
+                self.create_oval(coord2, outline="#FF8B8B")
+                self.create_oval(coord1, fill="#FF2E2E", outline="#FF8B8B")
+                self.create_oval(coord1, outline="#FF8B8B")
+                text = self.create_text(130, 130, text=int(txt_consigne), font="Arial 40 italic", fill="#FF8B8B")
                 parent.update()
-                #time.sleep(0.00002)    #DÃ©finie l'inertie de la gauge
+                #time.sleep(0.00002)    #Définie l'inertie de la gauge
 
             elif( int(valeur) > int(consigne*3.6)):
                 valeur = valeur - 1
                 txt_consigne = valeur/3.6
                 self.delete(arc)
                 self.delete(text)
-                arc = self.create_arc((H-240), (W-240), (H-20), (W-20), start=90, extent=-valeur, fill="#3399FF",outline="#3399FF")
-                self.create_oval((H-210), (W-215), (H-50), (W-50), fill="#3300FF", outline="#3300FF")
-                text = self.create_text(130, 130, text=int(txt_consigne), font="Arial 40 italic", fill="#3399FF")
+
+                arc = self.create_arc(coord2, start=90, extent=-valeur, fill="#FF8B8B")
+                self.create_oval(coord2, outline="#FF8B8B")
+                self.create_oval(coord1, fill="#FF2E2E", outline="#FF8B8B")
+                self.create_oval(coord1, outline="#FF8B8B")
+                text = self.create_text(130, 130, text=int(txt_consigne), font="Arial 40 italic", fill="#FF8B8B")
                 parent.update()
-                #time.sleep(0.00002)    #DÃ©finie l'inertie de la gauge
+                #time.sleep(0.00002)    #Définie l'inertie de la gauge
                 
             else :
                 txt_consigne = valeur/3.6
                 self.delete(arc)
                 self.delete(text)
-                arc = self.create_arc((H-240), (W-240), (H-20), (W-20), start=90, extent=-valeur, fill="#3399FF",outline="#3399FF")
-                self.create_oval((H-210), (W-215), (H-50), (W-50), fill="#3300FF", outline="#3300FF")
-                text = self.create_text(130, 130, text=int(txt_consigne), font="Arial 40 italic", fill="#3399FF")
+                arc = self.create_arc(coord2, start=90, extent=-valeur, fill="#FF8B8B")
+                self.create_oval(coord2, outline="#FF8B8B")
+                self.create_oval(coord1, fill="#FF2E2E", outline="#FF8B8B")
+                self.create_oval(coord1, outline="#FF8B8B")
+                text = self.create_text(130, 130, text=int(txt_consigne), font="Arial 40 italic", fill="#FF8B8B")
                 parent.update()
-                #time.sleep(0.00002)    #DÃ©finie l'inertie de la gauge
+                #time.sleep(0.00002)    #Définie l'inertie de la gauge
 
 def val():
     for i in range(1,10):
-        gauge.SetValue(randrange(10))
+        gauge.SetValue(randrange(100))
 
 if __name__=="__main__":
     app=Tkinter.Tk()
